dcb9f1a1911346b8491631fc7609b5eb4f722fef
k21971 <keith.simpson1971@gmail.com>
2026-02-12 00:20:23 +0000

    Make $ATTR() banner colors use the terminal's default background instead of forcing black.
    Not everyone's terminal uses a black background. This can make various
    text look out of place, especially for those using the default macOS
    terminal (white background/black foreground text color). Means I'll need
    to identify any $ATTR(15) settings (white) that I use on Hardfought and
    change that to something else - stay away from black and white to cater
    to most all terminal types.

--- dgamelaunch.c
+++ dgamelaunch.c
@@ -2187,16 +2187,16 @@ initcurses ()
   start_color();
   use_default_colors();
 
-  init_pair(COLOR_BLACK, COLOR_WHITE, COLOR_BLACK);
-  init_pair(COLOR_RED, COLOR_RED, COLOR_BLACK);
-  init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_BLACK);
-  init_pair(COLOR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
-  init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLACK);
-  init_pair(COLOR_MAGENTA, COLOR_MAGENTA, COLOR_BLACK);
-  init_pair(COLOR_CYAN, COLOR_CYAN, COLOR_BLACK);
-  init_pair(COLOR_WHITE, COLOR_WHITE, COLOR_BLACK);
-  init_pair(9, 0, COLOR_BLACK);
-  init_pair(10, COLOR_BLACK, COLOR_BLACK);
+  init_pair(COLOR_BLACK, COLOR_WHITE, -1);
+  init_pair(COLOR_RED, COLOR_RED, -1);
+  init_pair(COLOR_GREEN, COLOR_GREEN, -1);
+  init_pair(COLOR_YELLOW, COLOR_YELLOW, -1);
+  init_pair(COLOR_BLUE, COLOR_BLUE, -1);
+  init_pair(COLOR_MAGENTA, COLOR_MAGENTA, -1);
+  init_pair(COLOR_CYAN, COLOR_CYAN, -1);
+  init_pair(COLOR_WHITE, COLOR_WHITE, -1);
+  init_pair(9, 0, -1);
+  init_pair(10, COLOR_BLACK, -1);
   init_pair(11, -1, -1);
 
   if (globalconfig.utf8esc) {
